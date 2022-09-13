e9cda94d409b52d3c6718dd50766113a084d486e
jguhlin <joseph.guhlin@gmail.com>
2022-09-14 08:22:35 +1200

    Update prelude.rs

--- libsfasta/src/prelude.rs
+++ libsfasta/src/prelude.rs
@@ -4,5 +4,6 @@ pub use crate::datatypes::structs::CompressionType;
 pub use crate::datatypes::Sequence;
 pub use crate::dual_level_index::*;
 pub use crate::formats::fasta::*;
+pub use crate::formats::fastq::*;
 pub use crate::formats::sfasta::{SeqMode, Sequences, Sfasta, SfastaParser};
 pub use crate::masking::*;
