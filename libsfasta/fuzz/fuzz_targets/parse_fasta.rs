7df631c0d09a6573554969bd95656c9c1d999b40
jguhlin <joseph.guhlin@gmail.com>
2022-09-14 08:16:21 +1200

    Update parse_fasta.rs

--- libsfasta/fuzz/fuzz_targets/parse_fasta.rs
+++ libsfasta/fuzz/fuzz_targets/parse_fasta.rs
@@ -3,6 +3,6 @@ use libfuzzer_sys::fuzz_target;
 extern crate libsfasta;
 
 fuzz_target!(|data: &[u8]| {
-    let mut buf = std::io::BufReader::new(data.to_vec());
+    let mut buf = std::io::BufReader::new(data);
     let _ = libsfasta::prelude::Fasta::from_buffer(&mut buf);
 });
