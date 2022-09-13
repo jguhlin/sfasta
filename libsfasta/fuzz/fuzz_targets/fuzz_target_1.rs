73f5095660830592d9cfee23458d64545f3b8605
jguhlin <joseph.guhlin@gmail.com>
2022-09-13 21:30:22 +1200

    Prepare to add fuzzing again

--- libsfasta/fuzz/fuzz_targets/fuzz_target_1.rs
+++ libsfasta/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,6 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
+extern crate libsfasta;
 
 fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
+    let mut buf = data.to_vec();
+    let _ = libsfasta::prelude::Fasta::from_buffer(&mut buf);
 });
