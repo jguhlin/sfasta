dd162737155735a047b071a5360d352ad3f8b445
jguhlin <joseph.guhlin@gmail.com>
2022-09-14 13:02:22 +1200

    Update parse_fasta.rs

--- libsfasta/fuzz/fuzz_targets/parse_fasta.rs
+++ libsfasta/fuzz/fuzz_targets/parse_fasta.rs
@@ -6,6 +6,6 @@ fuzz_target!(|data: &[u8]| {
     let mut buf = std::io::BufReader::new(data);
     let fasta = libsfasta::prelude::Fasta::from_buffer(&mut buf);
     while let Some(_) = fasta.next() {
-        true
+        true;
     }
 });
