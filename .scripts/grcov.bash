297bc48f61f2207737904c6978ece8dbd852bb5c
jguhlin <joseph.guhlin@gmail.com>
2022-12-19 08:41:05 +1300

    Update grcov.bash

--- .scripts/grcov.bash
+++ .scripts/grcov.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+export RUSTC_BOOTSTRAP=1
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Cinstrument-coverage"
-export RUSTDOCFLAGS="-Cpanic=abort"
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"export RUSTDOCFLAGS="-Cpanic=abort"
 
 cargo build
 cargo test
 
-grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
+grcov ./target/debug/ -s . -t lcov --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
+genhtml -o ./target/debug/coverage/ --show-details --highlight --ignore-errors source --legend ./target/debug/lcov.info
