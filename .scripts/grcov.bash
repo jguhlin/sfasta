29ae16560890d0ee8680eeea253dfaf2de6d7266
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-12-19 09:31:59 +1300

    grcov not yet working

--- .scripts/grcov.bash
+++ .scripts/grcov.bash
@@ -2,10 +2,11 @@
 
 export RUSTC_BOOTSTRAP=1
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"export RUSTDOCFLAGS="-Cpanic=abort"
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTDOCFLAGS="-Cpanic=abort"
+export LLVM_PROFILE_FILE="sfasta-%p-%m.profraw"
 
 cargo build
 cargo test
 
-grcov ./target/debug/ -s . -t lcov --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
-genhtml -o ./target/debug/coverage/ --show-details --highlight --ignore-errors source --legend ./target/debug/lcov.info
+# grcov . -s . --binary-path ./target/debug/ -t lcov --branch --ignore-not-existing -o ./target/debug/coverage/
