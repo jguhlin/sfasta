79a3415124c124bf622aa0da75e2abe6128dd872
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-12-19 09:36:14 +1300

    Maybe working

--- .scripts/grcov.bash
+++ .scripts/grcov.bash
@@ -6,7 +6,7 @@ export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Co
 export RUSTDOCFLAGS="-Cpanic=abort"
 export LLVM_PROFILE_FILE="sfasta-%p-%m.profraw"
 
-cargo build
 cargo test
 
-# grcov . -s . --binary-path ./target/debug/ -t lcov --branch --ignore-not-existing -o ./target/debug/coverage/
+#grcov . -s . --binary-path ./target/debug/ -t lcov --branch --ignore-not-existing -o ./target/debug/coverage/
+grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/tests.lcov
