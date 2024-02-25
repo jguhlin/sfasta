efe9d6cf70f1e1ead22f3a047502bb498196a655
Joseph Guhlin <joseph.guhlin@gmail.com>
2024-02-25 21:11:23 +1300

    Update lib.rs

--- bptree/src/lib.rs
+++ bptree/src/lib.rs
@@ -147,7 +147,8 @@ impl<K, V> Node<K, V> {
                 .map(|k| *k > key)
                 .position(|x| x)
                 .unwrap_or(self.keys.len() - 1); */
-            let mut i = self.keys.binary_search(&key).unwrap_or_else(|x| x);
+            let arch = Arch::new();
+            let mut i = arch.dispatch(|| self.keys.binary_search(&key).unwrap_or_else(|x| x));
             if i == self.keys.len() {
                 i -= 1;
             }
