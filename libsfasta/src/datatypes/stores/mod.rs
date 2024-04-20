89fade104ad8df25e16d63d13ca42ce0519d75a6
Joseph Guhlin <joseph.guhlin@gmail.com>
2024-04-20 13:20:00 +1200

    ThreadBuilder tests

--- libsfasta/src/datatypes/stores/mod.rs
+++ libsfasta/src/datatypes/stores/mod.rs
@@ -12,8 +12,6 @@ pub use masking::*;
 pub use string_block_store::*;
 pub use threads::*;
 
-pub(crate) type LocMutex = Arc<Mutex<(bool, Vec<Loc>)>>;
 type Queue = (Sender<(LocMutex, Vec<u8>)>, Receiver<(LocMutex, Vec<u8>)>);
 
-/// First is new data, second is shutdown
-type Wakeup = Arc<(Mutex<(bool, bool)>, Condvar)>;
+pub(crate) type LocMutex = Arc<Mutex<(bool, Vec<Loc>)>>;
--- libsfasta/src/datatypes/stores/threads.rs
+++ libsfasta/src/datatypes/stores/threads.rs
@@ -1,6 +1,8 @@
-use super::*;
 use flume::{Receiver, Sender};
-use std::sync::{Arc, Condvar, Mutex};
+use crossbeam::utils::Backoff;
+
+use super::*;
+use std::sync::{Arc, Condvar, Mutex, atomic::{AtomicBool, Ordering}};
 use std::thread::JoinHandle;
 
 pub(crate) trait Builder<T>
@@ -16,7 +18,7 @@ where
     T: Send + Sync,
 {
     builder: Arc<Mutex<dyn Builder<T> + Send + Sync>>,
-    wakeup: Wakeup,
+    shutdown: Arc<AtomicBool>,
     pub sender: Sender<(LocMutex, T)>,
     worker: Option<JoinHandle<()>>,
 }
@@ -29,20 +31,27 @@ where T: Send + Sync + 'static {
 
         let (sender, receiver) = flume::bounded::<(LocMutex, T)>(32);
         let receiver = Arc::new(receiver);
-        let wakeup = Arc::new((Mutex::new((false, false)), Condvar::new()));
+        let shutdown_flag = Arc::new(AtomicBool::new(false));
 
         let receiver_worker = Arc::clone(&receiver);
         let builder_worker = Arc::clone(&
             builder);
-        let wakeup_worker = Arc::clone(&wakeup);
+
+        let shutdown_flag_worker = Arc::clone(&shutdown_flag);
+
+
 
         let handle = std::thread::spawn(move || {
             let receiver = receiver_worker;
             let builder = builder_worker;
-            let wakeup = wakeup_worker;
+            let shutdown_flag = shutdown_flag_worker;
 
             // Safe to hold the lock the entire time, this is a dedicated thread
             let mut builder = builder.lock().unwrap();
+
+            println!("Thread spawned");
+
+            let backoff = Backoff::new();
 
             loop {
                 for (store_loc, data) in receiver.drain() {
@@ -54,16 +63,17 @@ where T: Send + Sync + 'static {
                 }
 
                 if receiver.is_empty() {
-                    let (guards, condvar) = &*wakeup;
-                    let guard = guards.lock().unwrap();
-
-                    if guard.1 && receiver.is_empty() && !guard.0 {
+                    if shutdown_flag.load(Ordering::SeqCst) {
+                        println!("Shutting down");
                         builder.finalize();
                         return;
-                    } else if !guard.0 || !receiver.is_empty() {
-                        // Lock until we have data
-                        // We are ok with spurious wakeups, as we don't expect to sleep often
-                        let _guard = condvar.wait(guard).expect("Failed to wait on condvar");
+                    } else if receiver.is_empty() {
+                        backoff.snooze();
+                        if backoff.is_completed() {
+                            std::thread::sleep(std::time::Duration::from_millis(128));
+                            backoff.reset();
+                        }
+
                     }
                 }
             }
@@ -71,14 +81,100 @@ where T: Send + Sync + 'static {
 
         Self {
             builder,
-            wakeup,
+            shutdown: shutdown_flag,
             sender,
             worker: Some(handle),
         }
     }
 
-    pub fn join(self) -> JoinHandle<()> {
-        self.worker.unwrap()
+    pub fn join(self) -> Result<(), std::boxed::Box<(dyn std::any::Any + Send + 'static)>> {
+        {
+            self.shutdown.store(true, Ordering::SeqCst);
+            
+        }
+        
+        self.worker.unwrap().join()
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use flume::RecvError;
+    use std::sync::atomic::{AtomicBool, Ordering};
+    use std::sync::Arc;
+    use std::thread::sleep;
+    use std::time::Duration;
+
+    struct TestBuilder {
+        data: Vec<u8>,
+        finalized: AtomicBool,
+    }
+
+    impl Builder<Vec<u8>> for TestBuilder {
+        fn add(&mut self, input: Vec<u8>) -> Result<Vec<Loc>, &str> {
+            self.data.extend(input);
+            Ok(vec![])
+        }
+
+        fn finalize(&mut self) {
+            self.finalized.store(true, Ordering::SeqCst);
+        }
+    }
+
+    #[test]
+    fn test_thread_builder() {
+        let builder = TestBuilder {
+            data: vec![],
+            finalized: AtomicBool::new(false),
+        };
+
+        let mut thread_builder = ThreadBuilder::new(builder);
+
+        println!("Thread builder created");
+
+        let data = vec![1, 2, 3, 4, 5];
+        let loc = Arc::new(Mutex::new((false, vec![])));
+        thread_builder.sender.send((Arc::clone(&loc), data)).unwrap();
+
+        sleep(Duration::from_millis(100));
+
+        let loc_return = loc.lock().unwrap();
+        assert_eq!(loc_return.0, true);
+        assert_eq!(loc_return.1, vec![]);
+
+        let data = vec![6, 7, 8, 9, 10];
+        let loc = Arc::new(Mutex::new((false, vec![])));
+        thread_builder.sender.send((Arc::clone(&loc), data)).unwrap();
+
+        sleep(Duration::from_millis(100));
+
+        let loc_return = loc.lock().unwrap();
+        assert_eq!(loc_return.0, true);
+        assert_eq!(loc_return.1, vec![]);
+
+        let data = vec![11, 12, 13, 14, 15];
+        let loc = Arc::new(Mutex::new((false, vec![])));
+        thread_builder.sender.send((Arc::clone(&loc), data)).unwrap();
+
+        sleep(Duration::from_millis(100));
+
+        let loc_return = loc.lock().unwrap();
+        assert_eq!(loc_return.0, true);
+        assert_eq!(loc_return.1, vec![]);
+
+        let data = vec![16, 17, 18, 19, 20];
+        let loc = Arc::new(Mutex::new((false, vec![])));
+        thread_builder.sender.send((Arc::clone(&loc), data)).unwrap();
+
+        sleep(Duration::from_millis(100));
+
+        let loc = loc.lock().unwrap();
+        assert_eq!(loc.0, true);
+        assert_eq!(loc.1, vec![]);
+
+        thread_builder.join().expect("Failed to join thread");
+
+    }
 }
