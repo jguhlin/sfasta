472cf9085f6168451c900fc9b6920b65d9e1ca1b
Joseph Guhlin <joseph.guhlin@gmail.com>
2024-07-08 11:31:43 +1200

    Starting python implementation

--- pysfasta/src/lib.rs
+++ pysfasta/src/lib.rs
@@ -1,14 +1,48 @@
+use std::sync::Arc;
+
+use libsfasta::prelude::*;
 use pyo3::prelude::*;
 
-/// List all IDs in an SFASTA file
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+// Async when supported...
+
+#[pyclass]
+struct Sfasta
+{
+    inner: Arc<libsfasta::prelude::Sfasta>,
+    runtime: Arc<tokio::runtime::Runtime>,
+}
+
+#[pymethods]
+impl Sfasta
+{
+    #[new]
+    fn new(path: &str, threads: usize) -> PyResult<Self>
+    {
+        let runtime = Arc::new(
+            match threads {
+                0 => tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap(),
+                _ => tokio::runtime::Builder::new_multi_thread()
+                    .worker_threads(threads)
+                    .enable_all()
+                    .build()
+                    .unwrap(),
+            });
+
+        let inner = Arc::new(runtime.block_on(async move {
+            open_from_file_async(path).await.unwrap()
+        }));
+
+        Ok(Sfasta { inner, runtime })
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn sfasta(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn sfasta(m: &Bound<'_, PyModule>) -> PyResult<()>
+{
+    m.add_class::<Sfasta>()?;
     Ok(())
 }
